31f9963de6a2205068b0d7c6839519f0151bde24
skarandeep <108695031+skarandeep@users.noreply.github.com>
2022-07-18 10:58:10 -0400

    L-02

--- app/src/main/java/com/androidtest/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/androidtest/coderswag/Controller/MainActivity.kt
@@ -2,11 +2,28 @@ package com.androidtest.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.translation.ViewTranslationRequest
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ListView
+import com.androidtest.coderswag.Model.Category
 import com.androidtest.coderswag.R
+import com.androidtest.coderswag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+        android.R.layout.simple_list_item_1,
+        DataService.categories)
+
+        val categoryListView = findViewById<ListView>(R.id.categoryListView)
+        categoryListView.adapter = adapter
+
     }
 }
--- app/src/main/java/com/androidtest/coderswag/Model/Category.kt
+++ app/src/main/java/com/androidtest/coderswag/Model/Category.kt
@@ -2,4 +2,7 @@ package com.androidtest.coderswag.Model
 
 class Category (val title: String, var image: String) {
 
+    override fun toString(): String {
+        return title
+    }
 }
