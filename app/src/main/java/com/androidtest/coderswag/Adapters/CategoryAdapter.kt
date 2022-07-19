c8910ba01190b108b8b9fa1c920f23c2bf72808c
skarandeep <108695031+skarandeep@users.noreply.github.com>
2022-07-19 10:26:51 -0400

    L-04 ViewHolder complete - all well till now

--- app/src/main/java/com/androidtest/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/androidtest/coderswag/Adapters/CategoryAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.androidtest.coderswag.Model.Category
 import com.androidtest.coderswag.R
 
@@ -14,19 +15,43 @@ class CategoryAdapter(val context: Context, val categories: List<Category>) : Ba
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
+        val holder: ViewHolder
 
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-        val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
-        println("Heavy computing")
+        if (convertView == null){
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+
+            holder = ViewHolder()       //initializing holder
+
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+        // these lines modified above    val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
+        //    val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
+
+            println("I exist for the first time!")
+
+            categoryView.tag = holder
+        } else {
+            //holder is already created
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+            println("Go green recycle!")
+        }
+
+     //   categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+     //   val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
+     //   val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
+       // println("Heavy computing")
 
         val category = categories[position]
 
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        categoryImage.setImageResource(resourceId)
-        println(resourceId)
+        // categoryImage.setImageResource(resourceId)
+        holder.categoryImage?.setImageResource(resourceId)
+        // println(resourceId)
 
-        categoryName.text = category.title
+        // categoryName.text = category.title
+        holder.categoryName?.text = category.title
         return categoryView
     }
 
@@ -46,4 +71,5 @@ class CategoryAdapter(val context: Context, val categories: List<Category>) : Ba
         var categoryImage: ImageView? = null
         var categoryName: TextView? = null
     }
+
 }
